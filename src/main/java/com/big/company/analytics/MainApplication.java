7ba17828892c49828b88b70eb5e7b31f4de21007
Andre <andre.domingues.faustino@gmail.com>
2024-04-07 16:13:54 +0200

    Fix maven

--- src/main/java/com/big/company/analytics/MainApplication.java
+++ src/main/java/com/big/company/analytics/MainApplication.java
@@ -1,9 +1,15 @@
 package com.big.company.analytics;
 
+import com.big.company.analytics.domain.Employee;
+import com.big.company.analytics.extractor.EmployeeDataExtractor;
+import com.big.company.analytics.extractor.FileExtractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainApplication
 {
     public static void main( String[] args )
     {
-
     }
 }
